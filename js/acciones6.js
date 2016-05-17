7a2b848ea5d13f22518eec009f31d60c2cc31442
jagomez7411 <aredcbtis16@hotmail.com>
2016-05-17 11:25:18 -0500

    9

--- js/acciones6.js
+++ js/acciones6.js
@@ -1,15 +1,15 @@
-// JavaScript Document
+//JavaScript
 
-$(document).ready(function(e) {
-document .addEventListener("deviceready",function(){
-	
-	$('#beep').tap(function(){
-		navigator.notification.beep (2);
-	});//tap beep
-	
-	$('#vibrar').tap(function (){
-		navigator.notification.vibrate (1000);
-	});//tap vibrar
-	
-},false);//deviceready
-});//ready
+$(document).ready(function(e){
+document.addEventListener("deviceready",function(){
+
+$('#beep').tap(function(){
+navigator.notification.beep(1);
+});//tap beep
+
+$('#vibrar').tap(function(){
+navigator.notification.vibrate(1000);
+});//tap vibrar
+
+},false); //deviceready
+}); //ready
